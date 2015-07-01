1b672496c8ab96305bc2b502fba7f7a3b63bee5d
davebshow <davebshow@gmail.com>
2015-07-01 17:51:19 -0400

    working on OO API, almost done

--- gremlinrestclient/commands.py
+++ gremlinrestclient/commands.py
@@ -1,13 +1,13 @@
 class CommandsMixin(object):
-    """Based on
-    https://github.com/escalant3/pyblueprints/blob/master/pyblueprints/base.py
+    """
+    Some useful commands for the Gremlin Server.
     """
 
-    def addVertex(self, label=None):
+    def add_vertex(self, label=None):
         """
         Adds a new vertex to the graph.
 
-        :params str lable: Node label (optional)
+        :params str label: Node label (optional)
 
         :returns: The created Vertex
         """
@@ -19,9 +19,10 @@ class CommandsMixin(object):
             script = """graph.addVertex()"""
             bindings = {}
         resp = self.execute(script, bindings=bindings)
-        return resp
+        vertex = self.make_elem(resp, Vertex)
+        return vertex
 
-    def getVertex(self, vid):
+    def vertex(self, vid):
         """Retrieves an existing vertex from the graph
         :params int vid: Node unique identifier
 
@@ -29,77 +30,144 @@ class CommandsMixin(object):
         script = """g.V(vid)"""
         bindings = {"vid": vid}
         resp = self.execute(script, bindings=bindings)
-        return resp
+        vertex = self.make_elem(resp, Vertex)
+        return vertex
 
-    def getVertices(self):
+    def make_elem(self, resp, elem_class):
+        try:
+            data = resp.data[0]
+        except IndexError:
+            elem = None
+        else:
+            elem = elem_class(data, self)
+        return elem
+
+    def vertices(self):
         """Returns an iterator with all the vertices"""
         script = """g.V()"""
         resp = self.execute(script)
-        return resp
+        vertices = [Vertex(v, self) for v in resp.data]
+        return vertices
 
-    def getEdge(self, eid):
+    def edge(self, eid):
         """Retrieves an existing edge from the graph
         :params str eid: Edge unique identifier
 
         :returns: The requested Edge or None"""
-        script = """g.E(eid)"""
-        bindings = {"eid": eid}
-        resp = self.execute(script, bindings=bindings)
-        return resp
+        # Bindings don't seem to work here...
+        script = """g.E(%s)""" % (eid)
+        resp = self.execute(script)
+        edge = self.make_elem(resp, Edge)
+        return edge
 
-    def getEdges(self):
+    def edges(self):
         """Returns an iterator with all the edges"""
         script = """g.E()"""
         resp = self.execute(script)
-        return resp
+        edges = [Edge(e, self) for e in resp.data]
+        return edges
 
 
-class Element():
+class Element(object):
     """An abstract class defining an Element object composed
     by a collection of key/value properties"""
 
-    def getProperty(self, key):
-        """Gets the value of the property for the given key
-        @params key: The key which value is being retrieved
+    def __init__(self, element_data, gdb):
+        self._gdb = gdb
+        self._eid = element_data["id"]
+        self._properties = element_data["properties"]
+        self._type = element_data["type"]
+        self._label = element_data["label"]
+        self._source = ""
 
-        @returns The value of the property with the given key"""
-        raise NotImplementedError("Method has to be implemented")
+    @property
+    def label(self):
+        return self._label
 
-    def getPropertyKeys(self):
+    @property
+    def id(self):
+        return self._eid
+
+    @property
+    def properties(self):
+        return self._properties
+
+    def _get_script(self):
+        raise NotImplementedError
+
+    def values(self, prop):
+        get_script, bindings = self._get_script()
+        script = """%selem.values(prop);""" % (get_script)
+        bindings.update({"prop": prop})
+        resp = self._gdb.execute(script, bindings=bindings)
+        try:
+            prop = resp.data[0]
+        except IndexError:
+            prop = None
+        return prop
+
+    def property(self, key, value=None):
+        """Gets/sets the value of the property for the given key
+
+        :param str key: The key which value is being retrieved.
+
+        :param str value: The value to set the property to (optional).
+            None by default.
+
+        :returns: The property associated with the key"""
+        if value is not None:
+            script, bindings = self._set_property_script(key, value)
+            resp = self._gdb.execute(script, bindings=bindings)
+            if self._type == "vertex":
+                output = self._gdb.make_elem(resp, Vertex)
+            else:
+                print(resp)
+                output = self._gdb.make_elem(resp, Edge)
+        else:
+            output = self.values(key)
+        return output
+
+    def _set_property_script(self, prop, value):
+        get_script, bindings = self._get_script()
+        script = """%selem.property(prop, val);""" % (get_script)
+        bindings.update({"prop": prop, "val": value})
+        return script, bindings
+
+    def keys(self):
         """Returns a set with the property keys of the element
 
-        @returns Set of property keys"""
-        raise NotImplementedError("Method has to be implemented")
+        :returns: Set of property keys"""
+        return self._properties.keys()
 
-    def setProperty(self, key, value):
-        """Sets the property of the element to the given value
-        @params key: The property key to set
-        @params value: The value to set"""
-        raise NotImplementedError("Method has to be implemented")
-
-    def getId(self):
+    def get_id(self):
         """Returns the unique identifier of the element
 
         @returns The unique identifier of the element"""
-        raise NotImplementedError("Method has to be implemented")
-
-    def removeProperty(self, key):
-        """Removes the value of the property for the given key
-        @params key: The key which value is being removed"""
-        raise NotImplementedError("Method has to be implemented")
+        return self._eid
 
     def remove(self):
-        pass
+        get_script, bindings = self._get_script()
+        script = """%s;elem.remove;""" % (get_script)
+        self._gdb.execute(script, binding=bindings)
 
 
 class Vertex(Element):
     """An abstract class defining a Vertex object representing
     a node of the graph with a set of properties"""
 
-    def addEdge(self):
+    def __init__(self, vertex, gdb):
+        super(Vertex, self).__init__(vertex, gdb)
+        self._source = "V"
+
+    def _get_script(self):
+        script = """elem = g.%s(eid);""" % (self._source)
+        bindings = {"eid": self._eid}
+        return script, bindings
+
+    def add_edge(self):
         pass
 
-    def getOutEdges(self, label=None):
+    def out_edges(self, label=None):
         """Gets all the outgoing edges of the node. If label
         parameter is provided, it only returns the edges of
         the given label
@@ -108,7 +176,7 @@ class Vertex(Element):
         @returns A generator function of edges"""
         raise NotImplementedError("Method has to be implemented")
 
-    def getInEdges(self, label=None):
+    def in_edges(self, label=None):
         """Gets all the incoming edges of the node. If label
         parameter is provided, it only returns the edges of
         the given label
@@ -117,7 +185,7 @@ class Vertex(Element):
         @returns A generator function of edges"""
         raise NotImplementedError("Method has to be implemented")
 
-    def getBothEdges(self, label=None):
+    def edges(self, label=None):
         """Gets all the edges of the node. If label
         parameter is provided, it only returns the edges of
         the given label
@@ -131,20 +199,35 @@ class Edge(Element):
     """An abstract class defining a Edge object representing
     a relationship of the graph with a set of properties"""
 
-    def getOutVertex(self):
+    def __init__(self, edge, gdb):
+        super(Edge, self).__init__(edge, gdb)
+        self._source = "E"
+        self._out_vertex = edge["outV"]
+        self._in_vertex = edge["inV"]
+        self._out_label = edge["outVLabel"]
+        self._out_label = edge["inVLabel"]
+
+    def _get_script(self):
+        # A hack because bindings aren't working with edge lookups
+        script = """elem = g.%s(%s);""" % (self._source, self._eid)
+        bindings = {}
+        return script, bindings
+
+    def out_vertex(self):
         """Returns the origin Vertex of the relationship
 
-        @returns The origin Vertex"""
-        raise NotImplementedError("Method has to be implemented")
+        :returns: The origin Vertex"""
+        return self._get_vertex(self._out_vertex)
 
-    def getInVertex(self):
+    def in_vertex(self):
         """Returns the target Vertex of the relationship
 
-        @returns The target Vertex"""
-        raise NotImplementedError("Method has to be implemented")
+        :returns: The target Vertex"""
+        return self._get_vertex(self._in_vertex)
 
-    def getLabel(self):
-        """Returns the label of the relationship
-
-        @returns The edge label"""
-        raise NotImplementedError("Method has to be implemented")
+    def _get_vertex(self, vid):
+        script = """g.V(vid)"""
+        bindings = {"vid": vid}
+        resp = self._gdb.execute(script, bindings=bindings)
+        vertex = self._gdb.make_elem(resp, Vertex)
+        return vertex
--- tests/tests.py
+++ tests/tests.py
@@ -31,36 +31,76 @@ class GraphDatabaseCommandsTestCase(unittest.TestCase):
         self.client = GraphDatabase()
 
     def test_create_node(self):
-        resp = self.client.addVertex()
+        resp = self.client.add_vertex()
         # print(resp)
 
     def test_create_node_label(self):
-        resp = self.client.addVertex("test")
+        resp = self.client.add_vertex("test")
         # print(resp)
 
     def test_get_vertex(self):
-        resp = self.client.getVertex(1)
-        print(resp)
+        v = self.client.vertex(1)
+        self.assertEqual(v.id, 1)
 
     def test_get_vertex_id_doesntexist(self):
-        resp = self.client.getVertex(100)
-        print(resp)
+        v = self.client.vertex(10000000)
+        self.assertIsNone(v)
 
     def test_get_vertices(self):
-        resp = self.client.getVertices()
-        print(resp)
+        vs = self.client.vertices()
+        self.assertIsNotNone(vs)
 
     def test_get_edge(self):
-        resp = self.client.getEdge(1)
-        print(resp)
+        resp = self.client.edge(7)
+        # print(resp)
 
     def test_get_edge_id_doesntexist(self):
-        resp = self.client.getEdge(100)
-        print(resp)
+        resp = self.client.edge(10000)
+        # print(resp)
 
     def test_get_edges(self):
-        resp = self.client.getEdges()
-        print(resp)
+        resp = self.client.edges()
+        # print([r.id for r in resp])
+
+    def test_get_node_property(self):
+        v = self.client.vertex(1)
+        p = v.property('name')
+        self.assertEqual(p, "marko")
+
+    def test_set_node_property(self):
+        v = self.client.vertex(1)
+        p = v.property('name', 'dave')
+        v = self.client.vertex(1)
+        p = v.property('name')
+        self.assertEqual(p, "dave")
+        p = v.property('name', 'marko')
+        v = self.client.vertex(1)
+        p = v.property('name')
+        self.assertEqual(p, "marko")
+
+    def test_get_edge_property(self):
+        e = self.client.edge(7)
+        p = e.property('weight')
+        self.assertEqual(p, 0.5)
+
+    def test_set_node_property(self):
+        e = self.client.edge(7)
+        e.property('weight', 1.0)
+        e = self.client.edge(7)
+        p = e.property('weight')
+        self.assertEqual(p, 1.0)
+        e = self.client.edge(7)
+        e.property('weight', 0.5)
+        e = self.client.edge(7)
+        p = e.property('weight')
+        self.assertEqual(p, 0.5)
+
+    def test_edge_in_out_vertex(self):
+        edge = self.client.edge(7)
+        out = edge.out_vertex()
+        self.assertEqual(out.id, 1)
+        inv = edge.in_vertex()
+        self.assertEqual(inv.id, 2)
 
 
 if __name__ == "__main__":
